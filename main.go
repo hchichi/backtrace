5bce0d398d860f0ec30277f160e017ffdad5578f
yuumi <126152485+yuumi@users.noreply.github.com>
2025-01-07 10:47:42 +0800

    修复: 移动主程序到根目录，简化项目结构

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	backtrace "github.com/oneclickvirt/backtrace/bk"
+)
+
+var (
+	version   = "dev"
+	buildTime = "unknown"
+)
+
+func main() {
+	var (
+		showVersion bool
+		showHelp    bool
+		enableLog   bool
+		showIP      bool
+	)
+
+	flag.BoolVar(&showVersion, "v", false, "Show version")
+	flag.BoolVar(&showHelp, "h", false, "Show help information")
+	flag.BoolVar(&enableLog, "e", false, "Enable logging")
+	flag.BoolVar(&showIP, "s", true, "Disabe show ip info")
+	flag.Parse()
+
+	if showHelp {
+		fmt.Printf("Usage: backtrace [options]\n")
+		flag.PrintDefaults()
+		return
+	}
+
+	if showVersion {
+		fmt.Printf("Version: %s\nBuild Time: %s\n", version, buildTime)
+		return
+	}
+
+	backtrace.BackTrace()
+}
